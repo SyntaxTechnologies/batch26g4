bc6cc34b042265a07b795fae8fff97cb6c616e28
Kh84249 <kshoaiby@gmail.com>
2026-07-15 11:34:53 +0200

    My first project

---
+++ src/test/java/SolarSystem/S/Basics.java
@@ -0,0 +1,12 @@
+package SolarSystem.S;
+
+public class Basics {
+
+    String name;
+    String lastName;
+    String gender;
+    String address;
+    String city;
+    String state;
+    String country;
+}
